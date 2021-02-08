e0debae4896e6ee4b05384db274b8b72d6c2ea95
simata98 <wlsrnr905@naver.com>
2021-02-08 23:23:08 +0900

    nodeJS practice

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# MEN 주소록
-이 저장소의 코드는
-[http://a-mean-blog.com/ko/blog/Node-JS-첫걸음/주소록-만들기](http://a-mean-blog.com/ko/blog/Node-JS-첫걸음/주소록-만들기)에서 작성되었습니다.
-<br>
+주소록 nodeJS 연습
--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-var express = require('express');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var app = express();
 
 // DB setting
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect(process.env.MONGO_DB);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(
+  "mongodb+srv://admin:wlsrnr905!@cluster0.rf2ch.mongodb.net/contactBook?retryWrites=true&w=majority"
+);
 var db = mongoose.connection;
-db.once('open', function(){
-  console.log('DB connected');
+db.once("open", function () {
+  console.log("DB connected");
 });
-db.on('error', function(err){
-  console.log('DB ERROR : ', err);
+db.on("error", function (err) {
+  console.log("DB ERROR : ", err);
 });
 
 // Other settings
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname+'/public'));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// DB schema
+var contactSchema = mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String },
+  phone: { type: String },
+});
+var Contact = mongoose.model("contact", contactSchema);
+
+// Routes
+// Home
+app.get("/", function (req, res) {
+  res.redirect("/contacts");
+});
+// Contacts - Index
+app.get("/contacts", function (req, res) {
+  Contact.find({}, function (err, contacts) {
+    if (err) return res.json(err);
+    res.render("contacts/index", { contacts: contacts });
+  });
+});
+// Contacts - New
+app.get("/contacts/new", function (req, res) {
+  res.render("contacts/new");
+});
+// Contacts - create
+app.post("/contacts", function (req, res) {
+  Contact.create(req.body, function (err, contact) {
+    if (err) return res.json(err);
+    res.redirect("/contacts");
+  });
+});
 
 // Port setting
 var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+port);
+app.listen(port, function () {
+  console.log("server on! http://localhost:" + port);
 });
